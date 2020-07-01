ee3071b2458d488bb383a0fa22869ad008333c76
Alex Duvanenko <ajduvanenko@gmail.com>
2020-07-01 18:14:18 -0400

    Added nodemon, express-handlebars, basic routing, basic partials, basic layout, and basic templates

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const express = require('express');
-const app = express();
-const port = 3000;
+const exphbs = require('express-handlebars');
 
-app.get('/', (req, res) => res.send('Hello World!'));
+const app = express();
+const port = 8080;
+const domain = 'doovy.gg';
+
+app.engine('.html', exphbs({
+    defaultLayout: 'main',
+    extname: '.html'
+}));
+app.set('view engine', '.html');
+
+// routing
+
+app.get('/', (req, res) => {
+    res.render('index', {title: `Home - ${domain}`});
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {title: `About - ${domain}`});
+});
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
