a8d1aa90120a3d86308dd76b399969bad81bc104
Alex Duvanenko <ajduvanenko@gmail.com>
2020-07-02 01:09:04 -0400

    removed node-sass again, readded bootstrap

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ app.engine('.html', exphbs({
 
 app.set('view engine', '.html');
 
+app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist/css/'));
+
 // routing
 
 app.get('/', (req, res) => {
